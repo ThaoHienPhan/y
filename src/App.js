8fe0542efb415759d7f760a49a9ec35f360ea678
ThaoHienPhan <hien096456789@gmail.com>
2022-04-25 17:37:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,162 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import img from "./img/Tiki.jpg";
 
 function App() {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [passwordError, setpasswordError] = useState("");
+  const [emailError, setemailError] = useState("");
+
+  const handleValidation = (event) => {
+    let formIsValid = true;
+
+    if (!email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+      formIsValid = false;
+      setemailError("Email Not Valid");
+      return false;
+    } else {
+      setemailError("");
+      formIsValid = true;
+    }
+
+    if (!password.match(/^[a-zA-Z]{8,22}$/)) {
+      formIsValid = false;
+      setpasswordError(
+        "Only Letters and length must best min 8 Chracters and Max 22 Chracters"
+      );
+      return false;
+    } else {
+      setpasswordError("");
+      formIsValid = true;
+    }
+
+    return formIsValid;
+  };
+
+  const loginSubmit = (e) => {
+    e.preventDefault();
+    handleValidation();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="left">
+          <div class="col bg-light text-dark">
+            <button type="submit" className="btn btn-light button1">
+              <h6>Dang nhap</h6>
+            </button>
+            <form id="loginform" onSubmit={loginSubmit}>
+              <div className="form-group">
+                <label>Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="EmailInput"
+                  name="EmailInput"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+                <small id="emailHelp" className="text-danger form-text">
+                  {emailError}
+                </small>
+              </div>
+              <div className="form-group">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+                <small id="passworderror" className="text-danger form-text">
+                  {passwordError}
+                </small>
+              </div>
+              <div className="form-group form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label className="form-check-label">Check me out</label>
+              </div>
+              <button type="submit" className="btn btn-danger">
+                Tiep Tuc
+              </button>
+              <div className="gglogin">Hoac dang nhap bang</div>
+              <div>
+                <button className="button2"></button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="right">
+          <img src={img} alt=""></img>
+        </div>
+      </div>
+
+      {/* <div className="row d-flex justify-content-center">
+          <div class="col bg-light text-dark">
+            
+            <button type="submit" className="btn btn-light button1">
+              <h6>Dang nhap</h6>
+            </button>
+            <form id="loginform" onSubmit={loginSubmit}>
+              <div className="form-group">
+                <label>Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="EmailInput"
+                  name="EmailInput"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+                <small id="emailHelp" className="text-danger form-text">
+                  {emailError}
+                </small>
+              </div>
+              <div className="form-group">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+                <small id="passworderror" className="text-danger form-text">
+                  {passwordError}
+                </small>
+              </div>
+              <div className="form-group form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label className="form-check-label">Check me out</label>
+              </div>
+              <button type="submit" className="btn btn-danger">
+                Tiep Tuc
+              </button>
+              <div className="gglogin">Hoac dang nhap bang</div>
+              <div>
+                <button className="button2"></button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div>
+          <img src={img} alt=""></img>
+        </div> */}
     </div>
   );
 }
-
 export default App;
